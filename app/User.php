456512154f8fc54c57f288e06145ac035d603d94
Pavel Rorecek <pavel@rorecek.com>
2019-09-12 19:11:34 +0200

    Update User model class

--- app/User.php
+++ app/User.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -15,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -24,11 +26,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     public function completedModules()
     {
-        return $this->belongsToMany('App\Module', 'user_completed_modules');
+        return $this->belongsToMany(Module::class, 'user_completed_modules');
     }
 }
