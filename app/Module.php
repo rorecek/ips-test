e33f5faf2418425257fa9e6de8c350301a2eda63
Pavel Rorecek <pavel@rorecek.com>
2019-09-13 00:07:07 +0200

    Add first and next module methods to Module model class

--- app/Module.php
+++ app/Module.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function firstModule($courseKey)
+    {
+        return static::where('course_key', $courseKey)
+            ->orderBy('module_number')
+            ->first();
+    }
+
+    public function nextModule()
+    {
+        return static::where('course_key', $this->course_key)
+            ->where('module_number', $this->module_number + 1)
+            ->first();
+    }
 }
